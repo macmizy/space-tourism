5eb5d6fb877a28cd6a5c9d1fe5702f83d67ef4b1
macmizy <macmizy@gmail.com>
2022-09-04 15:05:09 +0100

    processing

---
+++ script.js
@@ -0,0 +1,7 @@
+const menubtn = document.getElementById('menubtn')
+const menulist = document.getElementById('menulist') 
+
+menubtn.addEventListener("click",()=>{
+    menulist.classList.toggle("show")
+    menubtn.classList.toggle("active")
+})
